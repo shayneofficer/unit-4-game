bf4b7b9831cd4e67031d34620d6a7bdd8d9cca59
shayneofficer <shayneofficer@gmail.com>
2018-11-14 09:47:13 -0600

    added images and headings

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+var obi = {
+    name: "Obi-wan Kenobi",
+    health: 120,
+    attack: 8,
+    counter: 20
+}
+
+var luke = {
+    name: "Luke Skywalker",
+    health: 100,
+    attack: 8,
+    counter: 5
+}
+
+var sidious = {
+    name: "Darth Sidious",
+    health: 150,
+    attack: 8,
+    counter: 20
+}
+
+var maul = {
+    name: "Darth Maul",
+    health: 180,
+    attack: 8,
+    counter: 25
+}
